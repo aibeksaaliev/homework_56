b9c45f8865907a7d4f4310cb192be8ccf1d541c3
Aibek Saaliev <dokaidea@gmail.com>
2022-11-03 16:58:15 +0600

    added styles for ingredients menu and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import './App.css';
 import IngredientsMenu from "./components/IngredientsMenu/IngredientsMenu";
 import OrderWindow from "./components/OrderWindow/OrderWindow";
 import {Ingredient} from "./types";
-import meatImg from './assets/5b6a94cc-dee5-4165-b39d-c57b3047afb01667208332587.png';
-import cheeseImg from './assets/c24ac2fb-de6c-493b-9483-b8ee1aedabfb1667208333820.png';
-import saladImg from './assets/eacd261f-01ce-47eb-a2ba-4f99a728086b1667208333650.png';
-import baconImg from './assets/6b626fcf-c0eb-48d7-960d-d3a1d0b771431667208334353.png';
+import meatImg from './assets/image_2022-11-03_15-23-34.png';
+import cheeseImg from './assets/image_2022-11-03_15-24-14.png';
+import saladImg from './assets/image_2022-11-03_15-24-50.png';
+import baconImg from './assets/image_2022-11-03_15-24-34.png';
 
 const INGREDIENTS: Ingredient[] = [
   {name: "Meat", cost: 80, image: meatImg, id: 156},
